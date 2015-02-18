a624b3093631f91e5cc88f2d93aedbd527bd20f6
heatherf <heatherf@ca.ibm.com>
2015-02-18 09:06:45 -0500

    chmod +x and -p range not parsing

--- docker-run.sh
+++ docker-run.sh
@@ -14,7 +14,9 @@ docker run \
     -d --name mock_ccsapi \
     -e DOCKER_REMOTE_HOST="${BRIDGE_IP}:4243" \
     -e CCSAPI_URL="http://localhost:5000/v2.0/containers" \
-    -p 6000-6009:6000-6009 -p 5000:5000 \
+    -p 6000:6000 -p 6001:6001 -p 6002:6002 -p 6003:6003 -p 6004:6004 -p 6005:6005 -p 6006:6006 -p 6007:6007 -p 6008:6008 -p 6009:6009 \
+    -p 5000:5000 \
     xdevops/mock-ccsapi
 
+#    -p 6000-6009:6000-6009 -p 5000:5000 \
 #TODO    -p 6000-6009:6000-6009 -p 5101:5101 -p 5000:5000 \
